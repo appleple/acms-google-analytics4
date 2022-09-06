1c9f3c8f611437b964011bdf62cb38939cd46b89
宇井 陸登 <ui@appleple.com>
2022-09-06 11:11:24 +0900

    build: commit 時に LInt と Format が自動でされるように Git Hook を追加

--- GET/GoogleAnalytics4/Ranking.php
+++ GET/GoogleAnalytics4/Ranking.php
@@ -7,13 +7,14 @@ use ACMS_Corrector;
 use Template;
 
 /**
- *  Analytics Data APIと連携して、ページをアクセス数に基づいたランキング形式で表示するモジュール
- *  @see https://developers.google.com/analytics/devguides/reporting/data/v1
+ *  Analytics Data APIと連携して、
+ *  ページをアクセス数に基づいたランキング形式で表示するモジュール
  */
 class Ranking extends ACMS_GET
 {
     public function get()
     {
         $Tpl = new Template($this->tpl, new ACMS_Corrector());
+        $this->buildModuleField($Tpl);
     }
 }
--- Services/AnalyticsData.php
+++ Services/AnalyticsData.php
@@ -4,6 +4,7 @@ namespace Acms\Plugins\GoogleAnalytics4\Service;
 
 /**
  * Analytics Data APIのサービス
+ * @see https://developers.google.com/analytics/devguides/reporting/data/v1
  */
 class AnalyticsData
 {
