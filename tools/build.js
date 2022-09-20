4570c80bb9ac94ff1301b5a3ea4a22aebdfc1e43
宇井 陸登 <ui@appleple.com>
2022-09-20 18:25:25 +0900

    doc: VueでGoogleAnalytics4_Rankingを表示するサンプルコードを実装したサンプルテーマを追加

--- tools/build.js
+++ tools/build.js
@@ -20,6 +20,7 @@ co(function* () {
     fs.copySync('./Services', 'GoogleAnalytics4/Services');
     fs.copySync('./template', 'GoogleAnalytics4/template');
     fs.copySync('./vendor', 'GoogleAnalytics4/vendor');
+    fs.copySync('./themes', 'GoogleAnalytics4/themes');
     fs.copySync('./ServiceProvider.php', 'GoogleAnalytics4/ServiceProvider.php');
     yield zipPromise('GoogleAnalytics4', `./build/v${version}/GoogleAnalytics4.zip`);
     fs.copySync(`./build/v${version}/GoogleAnalytics4.zip`, './build/GoogleAnalytics4.zip');
